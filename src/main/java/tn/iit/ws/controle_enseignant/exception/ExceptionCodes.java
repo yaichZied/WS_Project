8d255c994ae4e974643cfbb9c595ab4089f7368e
Khaled Baklouti <khaledbaklouti8@gmail.com>
2017-12-22 14:05:59 +0100

    remove old project

--- src/main/java/tn/iit/ws/controle_enseignant/exception/ExceptionCodes.java
+++
@@ -1,7 +0,0 @@
-package tn.iit.ws.controle_enseignant.exception;
-
-public class ExceptionCodes 
-{
-	public final static String ID_NOT_EXISTANT="\005\011\021\006\t\n\t\011\014\013\003\n\n";
-	public final static String EXISTANT_ID="\005\014\011\005\t\n\t\004\019\004\006\n\n";
-}
