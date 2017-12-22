2b8ebbb58ac1d7a135b53a3f4268fd8c89021bfe
Khaled Baklouti <khaledbaklouti8@gmail.com>
2017-12-22 18:15:30 +0100

    stateless spring security with jwt

---
+++ src/main/java/tn/iit/ws/security/config/filters/JWTAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package tn.iit.ws.security.config.filters;
+
+import static tn.iit.ws.security.config.SecurityConstants.EXPIRATION_TIME;
+import static tn.iit.ws.security.config.SecurityConstants.HEADER_STRING;
+import static tn.iit.ws.security.config.SecurityConstants.SECRET;
+import static tn.iit.ws.security.config.SecurityConstants.TOKEN_PREFIX;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import tn.iit.ws.entities.users.User;
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+    	return authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                		req.getParameter("username"),
+                		req.getParameter("password"))
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        String token = Jwts.builder()
+                .setSubject(((User) auth.getPrincipal()).getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
+                .compact();
+        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+    }
+}
