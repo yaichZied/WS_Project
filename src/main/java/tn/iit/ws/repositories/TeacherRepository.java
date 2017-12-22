16115cf311d762495c67d4c662650bc6e903229f
Khaled Baklouti <khaledbaklouti8@gmail.com>
2017-12-22 14:42:19 +0100

    creating teacher repo

---
+++ src/main/java/tn/iit/ws/repositories/TeacherRepository.java
@@ -0,0 +1,8 @@
+package tn.iit.ws.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface TeacherRepository extends JpaRepository<TeacherRepository, Long> {
+	
+}
