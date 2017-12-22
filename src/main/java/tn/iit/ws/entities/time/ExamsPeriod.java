8272030950c14cc85a7d3613157741f15b4b63ba
Khaled Baklouti <khaledbaklouti8@gmail.com>
2017-12-22 16:35:31 +0100

    repo creation

--- src/main/java/tn/iit/ws/entities/time/ExamsPeriod.java
+++ src/main/java/tn/iit/ws/entities/time/ExamsPeriod.java
@@ -1,25 +1,12 @@
 package tn.iit.ws.entities.time;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
-@EqualsAndHashCode(of="id")
-public class ExamsPeriod {
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String name;
-	private Date begin;
-	private Date end;
-	@ManyToOne
-	private Semester semester;
+@Getter @Setter
+public class ExamsPeriod extends InterruptionPeriod{
+	
 }
